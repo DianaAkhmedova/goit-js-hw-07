7bd360ef3fa035f43eec085d323686ec8e363109
DianaAkhmedova <106441711+DianaAkhmedova@users.noreply.github.com>
2022-11-10 22:38:07 +0200

    add markup

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const refs = {
+  list: document.querySelector(".gallery"),
+};
+console.log(refs.list);
+
+const markup = galleryItems
+  .map(
+    (item) => `<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`
+  )
+  .join("");
+
+refs.list.innerHTML = markup;
+console.log(refs.list);
